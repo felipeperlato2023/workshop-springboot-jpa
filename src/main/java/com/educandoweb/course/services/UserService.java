8ced669ad1162729ee323df86ae1b39954417f07
Felipe Perlato <fe.perlato@gmail.com>
2024-01-27 19:50:54 -0300

    Exception handling - update

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -12,6 +12,8 @@ import com.educandoweb.course.repositories.UserRepository;
 import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service // Indica que esta classe é um componente de serviço do Spring
 public class UserService {
 
@@ -48,11 +50,17 @@ public class UserService {
 	}  
 
 	public User update(Long id, User obj) {
+		try {
 		User entity = repository.getReferenceById(id);
 
 		update(entity, obj);
 
 		return repository.save(entity);
+		}catch(EntityNotFoundException e) {
+		
+			throw new ResourceNotFoundException(id);
+		}
+		
 	}
 
 	private void update(User entity, User obj) {
