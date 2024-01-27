a740ac7e81a1c63185914c0201aa48f498fa15e8
Felipe Perlato <fe.perlato@gmail.com>
2024-01-27 19:26:11 -0300

    Exception handling - delete

--- src/main/java/com/educandoweb/course/services/UserService.java
+++ src/main/java/com/educandoweb/course/services/UserService.java
@@ -2,11 +2,14 @@ package com.educandoweb.course.services;
 
 import java.util.List;
 
-import org.apache.el.stream.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+
 import com.educandoweb.course.entities.User;
 import com.educandoweb.course.repositories.UserRepository;
+import com.educandoweb.course.services.exceptions.DatabaseException;
 import com.educandoweb.course.services.exceptions.ResourceNotFoundException;
 
 @Service // Indica que esta classe é um componente de serviço do Spring
@@ -15,34 +18,40 @@ public class UserService {
 	@Autowired
 	private UserRepository repository;
 
-	 // Método para buscar todos os usuários no banco de dados
+	// Método para buscar todos os usuários no banco de dados
 	public List<User> findAll() {
-		return repository.findAll();  // Chama o método findAll() do UserRepository para obter todos os usuários
+		return repository.findAll(); // Chama o método findAll() do UserRepository para obter todos os usuários
 	}
-	
-	 // Método para buscar um usuário por ID no banco de dados
-	public User findById(long id)
-	{
+
+	// Método para buscar um usuário por ID no banco de dados
+	public User findById(long id) {
 		java.util.Optional<User> obj = repository.findById(id); // Busca um usuário pelo ID no UserRepository
-		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // Retorna o usuário encontrado (ou lança uma exceção se não for encontrado)
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id)); // Retorna o usuário encontrado (ou lança uma
+																			// exceção se não for encontrado)
 	}
-	
-	public User insert(User obj)
-	{
+
+	public User insert(User obj) {
 		return repository.save(obj);
 	}
-	
+
 	public void delete(Long id)
 	{
+		try{
 		repository.deleteById(id);
-	}
-	
-	public User update (Long id, User obj)
-	{
+		}catch(EmptyResultDataAccessException e) {
+		
+			throw new ResourceNotFoundException(id);
+		} catch(DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
+		
+	}  
+
+	public User update(Long id, User obj) {
 		User entity = repository.getReferenceById(id);
-		
-		update(entity,obj);
-		
+
+		update(entity, obj);
+
 		return repository.save(entity);
 	}
 
@@ -51,7 +60,6 @@ public class UserService {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
 		entity.setPhone(obj.getPhone());
-	
-		
+
 	}
 }
